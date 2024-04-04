3d7da0f04bec639c551caeb46ce11d81ae130ffc
elme-engineer <145334542+elme-engineer@users.noreply.github.com>
2024-04-04 16:44:34 +0100

    Update IGatewayCliente.java

--- src/IGatewayCliente.java
+++ src/IGatewayCliente.java
@@ -1,3 +1,7 @@
-public interface IGatewayCliente {
-    
+import java.rmi.*;
+
+public interface IGatewayCliente extends Remote{
+    StringBuilder sendURLIndex(StringBuilder url) throws RemoteException;
+    StringBuilder searchInput(StringBuilder input) throws RemoteException;
+
 }
